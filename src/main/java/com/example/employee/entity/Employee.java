65f00be0f61ccf58fcc9ec0a904e8e0d6e3df97f
Wu Xueheng <xhwu@thoughtworks.com>
2018-05-17 09:24:44 +0800

    Add constructor and getter/setter in Employee.

--- src/main/java/com/example/employee/entity/Employee.java
+++ src/main/java/com/example/employee/entity/Employee.java
@@ -1,5 +1,23 @@
 package com.example.employee.entity;
 
+import lombok.Data;
+
+@Data
 public class Employee {
 
+    private int id;
+    private String name;
+    private int age;
+    private String gender;
+    private int companyId;
+    private int salary;
+
+    public Employee(String name, int age, String gender, int salary, int id, int companyId) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.companyId = companyId;
+        this.salary = salary;
+    }
 }
