ccc0dea93700a7f6261de8bdc2a2e863c47d9a35
Wu Xueheng <xhwu@thoughtworks.com>
2018-05-18 07:54:40 +0800

    should_return_employee_name_when_employee_salary_is_max_and_given_company_id_ finished.

--- src/main/java/com/example/employee/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/repository/EmployeeRepository.java
@@ -15,6 +15,7 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     Employee findFirstEmployeeByNameContainingAndSalaryGreaterThan(String name, int salary);
 
     //3.找出一个薪资最高且公司ID是*的雇员以及该雇员的姓名
+    Employee findTopEmployeeByCompanyIdOrderBySalaryDesc(int companyId);
 
     //4.实现对Employee的分页查询，每页两个数据
 
--- src/test/java/com/example/employee/EmployeeJPATest.java
+++ src/test/java/com/example/employee/EmployeeJPATest.java
@@ -62,7 +62,12 @@ public class EmployeeJPATest {
     public void should_return_employee_name_when_employee_salary_is_max_and_given_company_id_() throws Exception {
         //3.找出一个薪资最高且公司ID是1的雇员以及该雇员的name
         Employee expectedEmployee = new Employee("xiaohong",19,"female",7000,1, 1);
+        Employee actualEmployee = employeeRepository.findTopEmployeeByCompanyIdOrderBySalaryDesc(1);
         String actualName = null;
+        if (actualEmployee != null) {
+            actualName = actualEmployee.getName();
+        }
+
         assertThat(actualName).isEqualTo(expectedEmployee.getName());
     }
 
